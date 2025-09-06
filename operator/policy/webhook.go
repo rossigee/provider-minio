0fa8f901a4a1d71788c3137a7733a952259a1e8a
Ross Golder <ross@golder.org>
2025-09-06 19:51:00 +0700

    fix(webhook): support both v1 and v1beta1 for user and policy validation
    - Add v1beta1 import to user and policy webhooks
    - Update ValidateCreate and ValidateUpdate to handle both API versions
    - Add separate validation functions for v1beta1 resources
    - Part 2 of webhook validation fix after bucket webhook (v0.16.8)
    
    Still need to fix ServiceAccount webhook in next commit

--- operator/policy/webhook.go
+++ operator/policy/webhook.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-logr/logr"
 	miniov1 "github.com/rossigee/provider-minio/apis/minio/v1"
+	miniov1beta1 "github.com/rossigee/provider-minio/apis/minio/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -20,26 +21,34 @@ type Validator struct {
 
 // ValidateCreate implements admission.CustomValidator.
 func (v *Validator) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
-	v.log.V(1).Info("Validate create")
-
-	policy, ok := obj.(*miniov1.Policy)
-	if !ok {
-		return nil, errNotPolicy
+	// Handle both v1 and v1beta1 API versions
+	if policyv1, ok := obj.(*miniov1.Policy); ok {
+		v.log.V(1).Info("Validate create v1")
+		return nil, v.validatePolicy(policyv1)
 	}
-
-	return nil, v.validatePolicy(policy)
+	
+	if policyv1beta1, ok := obj.(*miniov1beta1.Policy); ok {
+		v.log.V(1).Info("Validate create v1beta1")
+		return nil, v.validatePolicyV1Beta1(policyv1beta1)
+	}
+	
+	return nil, errNotPolicy
 }
 
 // ValidateUpdate implements admission.CustomValidator.
 func (v *Validator) ValidateUpdate(_ context.Context, _, newObj runtime.Object) (admission.Warnings, error) {
-	v.log.V(1).Info("Validate update")
-
-	newPolicy, ok := newObj.(*miniov1.Policy)
-	if !ok {
-		return nil, errNotPolicy
+	// Handle both v1 and v1beta1 API versions
+	if newPolicyv1, ok := newObj.(*miniov1.Policy); ok {
+		v.log.V(1).Info("Validate update v1")
+		return nil, v.validatePolicy(newPolicyv1)
 	}
-
-	return nil, v.validatePolicy(newPolicy)
+	
+	if newPolicyv1beta1, ok := newObj.(*miniov1beta1.Policy); ok {
+		v.log.V(1).Info("Validate update v1beta1")
+		return nil, v.validatePolicyV1Beta1(newPolicyv1beta1)
+	}
+	
+	return nil, errNotPolicy
 }
 
 // ValidateDelete implements admission.CustomValidator.
@@ -49,6 +58,18 @@ func (v *Validator) ValidateDelete(_ context.Context, obj runtime.Object) (admis
 }
 
 func (v *Validator) validatePolicy(policy *miniov1.Policy) error {
+	if policy.Spec.ForProvider.AllowBucket != "" && policy.Spec.ForProvider.RawPolicy != "" {
+		return fmt.Errorf(".spec.forProvider.allowBucket and .spec.forProvider.rawPolicy are mutual exclusive, please only specify one")
+	}
+
+	providerConfigRef := policy.Spec.ProviderConfigReference
+	if providerConfigRef == nil || providerConfigRef.Name == "" {
+		return field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+	}
+	return nil
+}
+
+func (v *Validator) validatePolicyV1Beta1(policy *miniov1beta1.Policy) error {
 	if policy.Spec.ForProvider.AllowBucket != "" && policy.Spec.ForProvider.RawPolicy != "" {
 		return fmt.Errorf(".spec.forProvider.allowBucket and .spec.forProvider.rawPolicy are mutual exclusive, please only specify one")
 	}
--- operator/user/webhook.go
+++ operator/user/webhook.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-logr/logr"
 	miniov1 "github.com/rossigee/provider-minio/apis/minio/v1"
+	miniov1beta1 "github.com/rossigee/provider-minio/apis/minio/v1beta1"
 	providerv1 "github.com/rossigee/provider-minio/apis/provider/v1"
 	"github.com/rossigee/provider-minio/operator/minioutil"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -33,63 +34,96 @@ type Validator struct {
 
 // ValidateCreate implements admission.CustomValidator.
 func (v *Validator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	v.log.V(1).Info("Validate create")
+	// Handle both v1 and v1beta1 API versions
+	if userv1, ok := obj.(*miniov1.User); ok {
+		v.log.V(1).Info("Validate create v1")
+		
+		providerConfigRef := userv1.Spec.ProviderConfigReference
+		if providerConfigRef == nil || providerConfigRef.Name == "" {
+			return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+		}
 
-	user, ok := obj.(*miniov1.User)
-	if !ok {
-		return nil, errNotUser
+		err := v.doesPolicyExist(ctx, userv1)
+		if err != nil {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), userv1.Spec.ForProvider.Policies, err.Error())
+		}
+		
+		return nil, nil
 	}
+	
+	if userv1beta1, ok := obj.(*miniov1beta1.User); ok {
+		v.log.V(1).Info("Validate create v1beta1")
+		
+		providerConfigRef := userv1beta1.Spec.ProviderConfigReference
+		if providerConfigRef == nil || providerConfigRef.Name == "" {
+			return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+		}
 
-	providerConfigRef := user.Spec.ProviderConfigReference
-	if providerConfigRef == nil || providerConfigRef.Name == "" {
-		return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+		err := v.doesPolicyExistV1Beta1(ctx, userv1beta1)
+		if err != nil {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), userv1beta1.Spec.ForProvider.Policies, err.Error())
+		}
+		
+		return nil, nil
 	}
-
-	err := v.doesPolicyExist(ctx, user)
-	if err != nil {
-		return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), user.Spec.ForProvider.Policies, err.Error())
-	}
-
-	return nil, nil
+	
+	return nil, errNotUser
 }
 
 // ValidateUpdate implements admission.CustomValidator.
 func (v *Validator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
-	v.log.V(1).Info("Validate update")
+	// Handle both v1 and v1beta1 API versions
+	if oldUserv1, ok := oldObj.(*miniov1.User); ok {
+		newUserv1 := newObj.(*miniov1.User)
+		v.log.V(1).Info("Validate update v1")
 
-	oldUser, ok := oldObj.(*miniov1.User)
-	if !ok {
-		return nil, errNotUser
-	}
-	newUser, ok := newObj.(*miniov1.User)
-	if !ok {
-		return nil, errNotUser
-	}
+		if newUserv1.GetUserName() != oldUserv1.GetUserName() {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "userName"), newUserv1.GetUserName(), "Changing the username is not allowed")
+		}
 
-	if newUser.GetUserName() != oldUser.GetUserName() {
-		return nil, field.Invalid(field.NewPath("spec", "forProvider", "userName"), newUser.GetUserName(), "Changing the username is not allowed")
-	}
+		providerConfigRef := newUserv1.Spec.ProviderConfigReference
+		if providerConfigRef == nil || providerConfigRef.Name == "" {
+			return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+		}
 
-	providerConfigRef := newUser.Spec.ProviderConfigReference
-	if providerConfigRef == nil || providerConfigRef.Name == "" {
-		return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
-	}
+		if newUserv1.GetDeletionTimestamp() != nil {
+			return nil, nil
+		}
 
-	// Deleting an object also triggers an update first to set the
-	// deletionTimestamp. If the policy and the user objects are deleted at the
-	// same time, it's possible that the policy has already been gone by the time
-	// the user update happens.
-	// So we ignore checking if the timestamp is set.
-	if newUser.GetDeletionTimestamp() != nil {
+		err := v.doesPolicyExist(ctx, newUserv1)
+		if err != nil {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), newUserv1.Spec.ForProvider.Policies, err.Error())
+		}
+
 		return nil, nil
 	}
+	
+	if oldUserv1beta1, ok := oldObj.(*miniov1beta1.User); ok {
+		newUserv1beta1 := newObj.(*miniov1beta1.User)
+		v.log.V(1).Info("Validate update v1beta1")
 
-	err := v.doesPolicyExist(ctx, newUser)
-	if err != nil {
-		return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), newUser.Spec.ForProvider.Policies, err.Error())
+		if newUserv1beta1.GetUserName() != oldUserv1beta1.GetUserName() {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "userName"), newUserv1beta1.GetUserName(), "Changing the username is not allowed")
+		}
+
+		providerConfigRef := newUserv1beta1.Spec.ProviderConfigReference
+		if providerConfigRef == nil || providerConfigRef.Name == "" {
+			return nil, field.Invalid(field.NewPath("spec", "providerConfigRef", "name"), "null", "Provider config is required")
+		}
+
+		if newUserv1beta1.GetDeletionTimestamp() != nil {
+			return nil, nil
+		}
+
+		err := v.doesPolicyExistV1Beta1(ctx, newUserv1beta1)
+		if err != nil {
+			return nil, field.Invalid(field.NewPath("spec", "forProvider", "policies"), newUserv1beta1.Spec.ForProvider.Policies, err.Error())
+		}
+
+		return nil, nil
 	}
-
-	return nil, nil
+	
+	return nil, errNotUser
 }
 
 // ValidateDelete implements admission.CustomValidator.
@@ -129,7 +163,39 @@ func (v *Validator) doesPolicyExist(ctx context.Context, user *miniov1.User) err
 	return nil
 }
 
+func (v *Validator) doesPolicyExistV1Beta1(ctx context.Context, user *miniov1beta1.User) error {
+	if len(user.Spec.ForProvider.Policies) == 0 {
+		return nil
+	}
+	config, err := getProviderConfigV1Beta1(ctx, user, v.kube)
+	if err != nil {
+		return err
+	}
+	ma, err := getMinioAdminFn(ctx, v.kube, config)
+	if err != nil {
+		return err
+	}
+	policies, err := ma.ListCannedPolicies(ctx)
+	if err != nil {
+		return err
+	}
+	for _, policy := range user.Spec.ForProvider.Policies {
+		_, ok := policies[policy]
+		if !ok {
+			return fmt.Errorf("policy not found: %s", policy)
+		}
+	}
+	return nil
+}
+
 func getProviderConfig(ctx context.Context, user *miniov1.User, kube client.Client) (*providerv1.ProviderConfig, error) {
+	configName := user.GetProviderConfigReference().Name
+	config := &providerv1.ProviderConfig{}
+	err := kube.Get(ctx, client.ObjectKey{Name: configName}, config)
+	return config, err
+}
+
+func getProviderConfigV1Beta1(ctx context.Context, user *miniov1beta1.User, kube client.Client) (*providerv1.ProviderConfig, error) {
 	configName := user.GetProviderConfigReference().Name
 	config := &providerv1.ProviderConfig{}
 	err := kube.Get(ctx, client.ObjectKey{Name: configName}, config)
